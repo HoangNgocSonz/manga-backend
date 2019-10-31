67abb3d7e7dc98ebe2c1735eb937a17b0bc0f4cf
HoangNgocSonz <hoangson20bg@gmail.com>
2019-10-31 21:02:40 +0700

    Update package.json

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 // const authRouter = require('./api/modules/auth/auth.router');
 
 mongoose.connect(config.mongoConnectionString);
-const PORT = 6969;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
